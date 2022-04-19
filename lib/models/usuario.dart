fac1b6ee96251cf04ae31500ce5a782554dde256
lucassouzapb <lycasoliveira@gmail.com>
2022-04-19 13:46:10 -0300

    Arrumar ícone principal do projeto #6

---
+++ lib/models/usuario.dart
@@ -0,0 +1,31 @@
+class Usuario {
+  late String _nome, _email, _senha, _funcao;
+
+  Usuario(this._nome, this._email, this._senha, this._funcao);
+
+  get funcao => _funcao;
+
+  set funcao(value) {
+    _funcao = value;
+  }
+
+  get senha => _senha;
+
+  set senha(value) {
+    _senha = value;
+  }
+
+  get email => _email;
+
+  set email(value) {
+    _email = value;
+  }
+
+  String get nome => _nome;
+
+  set nome(String value) {
+    _nome = value;
+  }
+
+
+}
