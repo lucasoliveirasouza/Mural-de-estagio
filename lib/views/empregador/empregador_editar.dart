dc5cbc783ed8b176c418e34879cb933b7dcd82f4
lucasoliveirasouza <>
2022-04-24 13:13:44 -0300

    criar interessados #25

---
+++ lib/views/empregador/empregador_editar.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class EmpregadorEditarView extends StatefulWidget {
+  const EmpregadorEditarView({Key? key}) : super(key: key);
+
+  @override
+  _EmpregadorEditarViewState createState() => _EmpregadorEditarViewState();
+}
+
+class _EmpregadorEditarViewState extends State<EmpregadorEditarView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/views/estudante/estudante_editar.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class EstudanteEditarView extends StatefulWidget {
+  const EstudanteEditarView({Key? key}) : super(key: key);
+
+  @override
+  _EstudanteEditarViewState createState() => _EstudanteEditarViewState();
+}
+
+class _EstudanteEditarViewState extends State<EstudanteEditarView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
