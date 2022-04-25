b59cef7b6d784cf00f5b58a2b7122444c2f3c153
lucassouzapb <lycasoliveira@gmail.com>
2022-04-25 07:24:39 -0300

    Criar edição de estudante no service #27

--- lib/views/estudante/estudante_editar.dart
+++ lib/views/estudante/estudante_editar.dart
@@ -1,6 +1,8 @@
-
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mural_estagio/services/estudante_service.dart';
+import 'package:mural_estagio/widgets/botao_padrao.dart';
+import 'package:mural_estagio/widgets/form_field_padrao.dart';
 
 class EstudanteEditarView extends StatefulWidget {
   const EstudanteEditarView({Key? key}) : super(key: key);
@@ -10,11 +12,58 @@ class EstudanteEditarView extends StatefulWidget {
 }
 
 class _EstudanteEditarViewState extends State<EstudanteEditarView> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  final nome = TextEditingController();
+  final email = TextEditingController();
+  final endereco = TextEditingController();
+  final telefone = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    EstudanteService()
+        .getEstudante(auth.currentUser!.email.toString())
+        .then((value) {
+      nome.text = value?.nome ?? "";
+      email.text = value?.email ?? "";
+      endereco.text = value?.endereco ?? "";
+      telefone.text = value?.telefone ?? "";
+    });
     return Scaffold(
       appBar: AppBar(
         title: Text("Editar estudante"),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 25, right: 10, left: 15),
+        child: ListView(
+          children: [
+            FormFieldPadrao(
+              controle: nome,
+              title: "Nome",
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            FormFieldPadrao(
+              controle: endereco,
+              title: "Endereço",
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            FormFieldPadrao(
+              controle: telefone,
+              title: "Telefone",
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            BotaoPadrao(
+              titulo: "Salvar",
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
