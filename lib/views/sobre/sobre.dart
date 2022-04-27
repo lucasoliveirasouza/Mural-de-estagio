d9973430288777930ffd0291771ed12d276a87b8
lucassouzapb <lycasoliveira@gmail.com>
2022-04-27 17:06:14 -0300

    Melhorar sobre #38

--- lib/views/sobre/sobre.dart
+++ lib/views/sobre/sobre.dart
@@ -42,14 +42,46 @@ class _SobreViewState extends State<SobreView> {
             ),
             Center(
               child: Text(
-                "Desenvolvido por Lucas Oliveira",
+                "Desenvolvido por Lucas Oliveira:",
                 style: TextStyle(
                   color: Colors.deepPurple,
                   fontSize: 15,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-            )
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: SizedBox(
+                    height: 30,
+                    width: 30,
+                    child: Image.asset("assets/images/gmail.png"),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: SizedBox(
+                    height: 30,
+                    width: 30,
+                    child: Image.asset("assets/images/instagram.png"),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: SizedBox(
+                    height: 30,
+                    width: 30,
+                    child: Image.asset("assets/images/whatsapp.png"),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
