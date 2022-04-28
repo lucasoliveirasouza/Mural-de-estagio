af773ba9f1d40e51fa9e1693fc0450ebd77ceb90
lucassouzapb <lycasoliveira@gmail.com>
2022-04-28 14:20:47 -0300

    Melhorar sobre #38

--- lib/views/vaga/vaga.dart
+++ lib/views/vaga/vaga.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mural_estagio/models/vaga.dart';
 import 'package:mural_estagio/widgets/botao_padrao.dart';
+import 'package:mural_estagio/widgets/linha_tabela.dart';
 
 class VagaView extends StatefulWidget {
   Vaga vaga;
@@ -19,22 +20,39 @@ class _VagaViewState extends State<VagaView> {
         title: Text("Detalhes da vaga"),
       ),
       body: Container(
-          padding: EdgeInsets.only(top: 20, left: 10, right: 10),
+          padding: EdgeInsets.only(top: 30, left: 15, right: 15),
           child: ListView(
             children: [
               Center(
                 child: Text(
                   "Oportunidade de estágio:",
                   style: TextStyle(
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.bold,
                     fontSize: 25,
                   ),
                 ),
               ),
-              ListTile(
-                title: Text("Empresa: " + widget.vaga.nomeEmpresa),
+              SizedBox(
+                height: 15,
               ),
-              ListTile(
-                title: Text("Cursos: " + widget.vaga.cursos),
+              LinhaTabela(
+                title: "Empresa:",
+                valor: widget.vaga.nomeEmpresa,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              LinhaTabela(
+                title: "Cursos:",
+                valor: widget.vaga.cursos,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              LinhaTabela(
+                title: "Remuneração:",
+                valor: "RS " + widget.vaga.remuneracao.toString(),
               ),
               ListTile(
                 title:
---
+++ lib/widgets/linha_tabela.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class LinhaTabela extends StatelessWidget {
+  String title;
+  String valor;
+
+  LinhaTabela({
+    Key? key,
+    required this.valor,
+    required this.title,
+  }) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: Table(
+        children: [
+          TableRow(children: [
+            Container(
+              padding: EdgeInsets.only(right: 5),
+              child: Text(
+                title,
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              color: Colors.deepPurple,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 5),
+              child: Text(
+                valor,
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.deepPurple,
+                ),
+              ),
+            )
+          ]),
+        ],
+      ),
+    );
+  }
+}
